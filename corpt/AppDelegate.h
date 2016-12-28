2a862462410dd031ff2d7b7e5f80820214d9f29b
wangyughost <136117373@qq.com>
2016-12-28 13:28:02 +0800

    First commit

---
+++ corpt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  corpt
+//
+//  Created by horen on 16/12/28.
+//  Copyright © 2016年 horen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ corpt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  corpt
+//
+//  Created by horen on 16/12/28.
+//  Copyright © 2016年 horen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
